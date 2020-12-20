98b32fb07f63ee3e88bee4cdd350876e86b676dd
Puneet Chandi <puneet.chandi@outlook.com>
2020-12-20 13:14:47 -0700

    addig my first java file to git

---
+++ array.java
@@ -0,0 +1,16 @@
+class array
+	{
+		public static void main(String argv[])
+		{int d=10;
+		int month[] = new int[12];
+		int year[] = {1991, 1992, 1993, 1994};
+		 for(int i=0; i<11; i++)
+			{month[i]=d;
+			d=d+10;}
+		for(int i=0; i<11; i++)
+		 System.out.println(month[i]);
+		 System.out.println("punnu singh");
+		for(int i=0; i<3; i++)
+		 System.out.println(year[i]);
+		}
+	}
